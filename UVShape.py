cd9ede671e555dc09cca9433d964d0fb95599f2e
Aliasguru <message@aliasguru.eu>
2017-01-09 14:57:26 +0100

    + move UV Panel to UI code

--- UVShape.py
+++ UVShape.py
@@ -347,23 +347,3 @@ class UpdateLineLengths(bpy.types.Operator):
 
 
 
-class Print3DTools(bpy.types.Panel):
-    """Creates a new tab with physics UI"""
-    bl_label = "3D Print Tools"
-    bl_idname = "3D Print Tools"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'TOOLS'
-    bl_category = "Extended Tools"
-
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column()
-        col.label(text = "UV Tools")
-        col.operator("object.shape_from_uv", text = "Create UV Shape", icon = 'SHAPEKEY_DATA')
-        #    col.operator("object.update_line_lengths", text="Update Measurements", icon='FILE_REFRESH')
-        #    col.prop(bpy.context.scene, "base_select_length", text="Base Select Length", icon='FORCE_HARMONIC')
-        #    col.prop(bpy.context.scene, "shape_select_length", text="Shape Select Length", icon='FORCE_HARMONIC')
-        #    col.prop(bpy.context.scene, "shape_base_difference", text="Difference", icon='FORCE_HARMONIC')
-        #    col.prop(bpy.context.scene, "shape_base_divisor", text="Divisor", icon='FORCE_HARMONIC')
-        #    if bpy.context.object != None:
-            #    col.prop(bpy.context.object, "relative_scale", text="Relative Scale", icon='FORCE_HARMONIC')
